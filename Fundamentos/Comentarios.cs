55e6eb0975c4c8d5c5a6dffbdde4d236a459991f
Lucas Ortigara Reis <lucas@gatilabs.com.br>
2020-01-17 17:04:25 -0300

    Exercícios de fundamentos para comentários, variáveis e constantes

---
+++ Fundamentos/Comentarios.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CursoCSharp.Fundamentos
+{
+    public class Comentarios
+    {
+        public static void Executar()
+        {
+            Console.WriteLine("Código claro é sempre melhor!");
+        }
+    }
+}
