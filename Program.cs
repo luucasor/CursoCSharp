adfb1cf2b9692eb8e4fc0155337ec7a0d6a15e45
Lucas Ortigara Reis <lucas@gatilabs.com.br>
2020-01-17 18:33:18 -0300

    Uso do caractere ? (interrogação), para proteger contra o erro de variável vazia

--- Program.cs
+++ Program.cs
@@ -11,6 +11,7 @@ namespace CursoCSharp {
                 {"Comentários - Fundamentos", Comentarios.Executar},
                 {"Variáveis e Constantes - Fundamentos", VariaveisEConstantes.Executar},
                 {"Interpolacao - Fundamentos", Interpolacao.Executar},
+                {"Notacao Ponto - Fundamentos", NotacaoPonto.Executar},
             });
 
             central.SelecionarEExecutar();
