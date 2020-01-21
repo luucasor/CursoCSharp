b29fc66472c120411c7600fc916edf1e0702da1f
Lucas Ortigara Reis <lucas@gatilabs.com.br>
2020-01-21 09:45:52 -0300

    Uso de operadores aritméticos.

--- Program.cs
+++ Program.cs
@@ -15,6 +15,7 @@ namespace CursoCSharp {
                 {"Lendo Dados - Fundamentos", LendoDados.Executar},
                 {"Formatando Número - Fundamentos", FormatandoNumero.Executar},
                 {"Conversões - Fundamentos", Conversoes.Executar},
+                {"Operadores Aritméticos - Fundamentos", OperadoresAritmeticos.Executar},
             });
 
             central.SelecionarEExecutar();
