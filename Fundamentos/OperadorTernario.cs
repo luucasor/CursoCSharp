f67abc99f15e96317afc68d02bf1026b0fc116ca
Lucas Ortigara Reis <lucas@gatilabs.com.br>
2020-01-21 16:20:41 -0300

    Uso do operador ternário

---
+++ Fundamentos/OperadorTernario.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CursoCSharp.Fundamentos
+{
+    public class OperadorTernario
+    {
+        public static void Executar()
+        {
+            var nota = 7.0;
+            var resultado = nota >= 7.0 ? "Aprovado" : "Reprovado";
+            Console.WriteLine(resultado);
+        }
+    }
+}
